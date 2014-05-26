39d9ec31f907613fea611a06267d14fbb7b88111
Calin Miclaus <calin.miclaus@uhurusoftware.com>
2014-05-26 12:23:55 +0300

    First commit

--- winnode/winnode.pp
+++ winnode/winnode.pp
@@ -1,6 +1,6 @@
 class vars {
-    $powershell = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
-    $tmpdir = 'X:/uhuru-origin-puppet'
+    $powershell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+    $tmpdir = "X:/uhuru-origin-puppet"
     $installerurl = "http://rpm.uhurucloud.net/origin-rpms/wininstaller/uhuru-openshift-winnode.exe"
 }
 
@@ -52,26 +52,27 @@ file { 'X:\uhuru-origin-puppet':
 }
 
 exec { 'UhuruOpenshift-download-installer':
-      require => Class['param'],
-      command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe iwr "${vars::installerurl}" -outfile "X:\uhuru-origin-puppet\uhuru-openshift-winnode.exe"',
-      creates => 'X:\uhuru-origin-puppet\uhuru-openshift-winnode.exe',
+      require => Class['param','vars'],
+      command => "${vars::powershell} iwr ${vars::installerurl} -outfile ${vars::tmpdir}/uhuru-openshift-winnode.exe",
+      creates => "${vars::tmpdir}/uhuru-openshift-winnode.exe",
      }
 
 exec { 'UhuruOpenshift-extract-installer':
-      require => Class['param'],
-      command => 'X:\uhuru-origin-puppet\uhuru-openshift-winnode.exe /T:X:\uhuru-origin-puppet\ /C /Q',
-      creates => 'X:\uhuru-origin-puppet\output.zip',
+      require => Class['param','vars'],
+      command => "${vars::tmpdir}/uhuru-openshift-winnode.exe /T:${vars::tmpdir} /C /Q",
+      creates => "${vars::tmpdir}/output.zip",
      }
 
 
 exec { 'UhuruOpenshift-run-packager':
-      require => Class['param'],
-      command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy bypass "X:\uhuru-origin-puppet\package.ps1" bootstrap',
+      require => Class['param','vars'],
+      command => "${vars::powershell} -ExecutionPolicy bypass ${vars::tmpdir}/package.ps1 bootstrap",
       creates => 'C:\openshift\installer\oo-cmd.exe',
      }
 
 exec { 'UhuruOpenshift-run-installer':
       require => Class['param'],
-      command => "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy bypass C:\openshift\installer\powershell\Tools\openshift.net\install.ps1 -publicHostname ${param::publicHostname} -brokerHost ${param::brokerHost} -cloudDomain ${param::cloudDomain} -sqlServerSAPassword ${param::sqlServerSAPassword} -externalEthDevice ${param::externalEthDevice} -internalEthDevice ${param::internalEthDevice} -publicIp ${param::publicIp} -gearBaseDir ${param::gearBaseDir} -gearShell ${param::gearShell} -gearGecos ${param::gearGecos} -cartridgeBasePath ${param::cartridgeBasePath} -platformLogFile ${param::platformLogFile} -platformLogLevel ${param::platformLogLevel} -containerizationPlugin ${param::containerizationPlugin} -rubyDownloadLocation ${param::rubyDownloadLocation} -rubyInstallLocation ${param::rubyInstallLocation} -mcollectiveActivemqServer ${param::mcollectiveActivemqServer} -mcollectiveActivemqPort ${param::mcollectiveActivemqPort} -mcollectiveActivemqUser ${param::mcollectiveActivemqUser} -mcollectiveActivemqPassword ${param::mcollectiveActivemqPassword} -mcollectivePskPlugin ${param::mcollectivePskPlugin} -sshdCygwinDir ${param::sshdCygwinDir} -sshdListenAddress ${param::sshdListenAddress} -sshdPort ${param::sshdPort} -proxy ${param::proxy} -skipRuby ${param::skipRuby} -skipCygwin ${param::skipCygwin} -skipMCollective ${param::skipMCollective} -skipChecks ${param::skipChecks} -skipGlobalEnv ${param::skipGlobalEnv} -skipServicesSetup ${param::skipServicesSetup} -skipBinDirCleanup ${param::skipBinDirCleanup}",
+      command => "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy bypass C:\\openshift\\installer\\powershell\\Tools\\openshift.net\\install.ps1 -publicHostname ${param::publicHostname} -brokerHost ${param::brokerHost} -cloudDomain ${param::cloudDomain} -sqlServerSAPassword ${param::sqlServerSAPassword} -externalEthDevice ${param::externalEthDevice} -internalEthDevice ${param::internalEthDevice} -publicIp ${param::publicIp} -gearBaseDir ${param::gearBaseDir} -gearShell ${param::gearShell} -gearGecos ${param::gearGecos} -cartridgeBasePath ${param::cartridgeBasePath} -platformLogFile ${param::platformLogFile} -platformLogLevel ${param::platformLogLevel} -containerizationPlugin ${param::containerizationPlugin} -rubyDownloadLocation ${param::rubyDownloadLocation} -rubyInstallLocation ${param::rubyInstallLocation} -mcollectiveActivemqServer ${param::mcollectiveActivemqServer} -mcollectiveActivemqPort ${param::mcollectiveActivemqPort} -mcollectiveActivemqUser ${param::mcollectiveActivemqUser} -mcollectiveActivemqPassword ${param::mcollectiveActivemqPassword} -mcollectivePskPlugin ${param::mcollectivePskPlugin} -sshdCygwinDir ${param::sshdCygwinDir} -sshdListenAddress ${param::sshdListenAddress} -sshdPort ${param::sshdPort} -proxy ${param::proxy} -skipRuby ${param::skipRuby} -skipCygwin ${param::skipCygwin} -skipMCollective ${param::skipMCollective} -skipChecks ${param::skipChecks} -skipGlobalEnv ${param::skipGlobalEnv} -skipServicesSetup ${param::skipServicesSetup} -skipBinDirCleanup ${param::skipBinDirCleanup}",
 #      creates => 'C:\openshift\installer\powershell\Tools\openshift.net',
      }
+
